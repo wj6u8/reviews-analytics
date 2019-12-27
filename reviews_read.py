9114b54b26e3e4ba70121b02ccf17626e2d4a4d2
Lianzhen <lianzhenw01@gmail.com>
2019-12-27 21:45:15 +0800

    calc ave length of review

--- reviews_read.py
+++ reviews_read.py
@@ -4,10 +4,11 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		if count % 1000 == 0:
+		if count % 100000 == 0:
 			print(len(data))
-print(len(data))
+print('讀取完畢,總共有', len(data), '筆資料')
 
-print(data[0])
-print('----------------------------------')
-print(data[10])
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+print('每筆留言平均用了', sum_len/len(data), '個字')
